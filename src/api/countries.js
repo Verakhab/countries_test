c2a177025b118267989426426fc0623e694366f9
Denis Bakharev <denis.bahareff@yandex.ru>
2024-04-23 17:40:38 +0300

    fix

--- src/api/countries.js
+++ src/api/countries.js
@@ -3,7 +3,7 @@ import config from '@/assets/config';
 import axios from 'axios';
 
 export async function getCountries(params) {
-    const url = `${config.development.hostname}/countries`;
+    const url = `${config.production.hostname}/countries`;
     let res = await axios.get(url, {
         params
     });
